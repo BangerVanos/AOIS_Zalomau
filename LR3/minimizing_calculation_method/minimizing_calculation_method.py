d3b921d5f879437ee823398e15fdaef16d1e8199
Roman <romchik349@gmail.com>
2023-03-18 14:21:57 +0300

    LR3 start

---
+++ LR3/minimizing_calculation_method/minimizing_calculation_method.py
@@ -0,0 +1,6 @@
+from fcnf_fdnf_converter.fcnf_fdnf_converter import FcnfFdnfFormConverter
+from logical_formula_solver.logical_formula_solver import LogicalFormulaSolver
+
+
+class CalculationMinimizer:
+    pass
