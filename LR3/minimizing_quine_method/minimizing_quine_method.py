89e53b62f5f82eceea8b4ef17738a223cc31cebc
Roman <romchik349@gmail.com>
2023-03-20 10:11:25 +0300

    Reorganized structure for LR3

--- LR3/minimizing_quine_method/minimizing_quine_method.py
+++ LR3/minimizing_quine_method/minimizing_quine_method.py
@@ -2,5 +2,9 @@ from fcnf_fdnf_converter.fcnf_fdnf_converter import FcnfFdnfFormConverter
 from logical_formula_solver.logical_formula_solver import LogicalFormulaSolver
 
 
-class CalculationMinimizer:
+class QuineCNFMinimizer:
+    pass
+
+
+class QuineDNFMinimizer:
     pass
