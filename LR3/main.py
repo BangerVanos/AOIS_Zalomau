e3e36cf4442274000f2bef140b2f67b3fc7d998f
Roman <romchik349@gmail.com>
2023-03-21 15:09:59 +0300

    Corrected Calculation method

--- LR3/main.py
+++ LR3/main.py
@@ -1,13 +1,19 @@
-from minimizing_calculation_method.minimizing_cnf_calculation_method import CalculationCNFMinimizer
-from minimizing_calculation_method.minimizing_dnf_calculation_method import CalculationDNFMinimizer
+from minimizing_calculation_method.minimizing_calculation_method import CalculationMinimizer
+
+
+formula = '(!((!x1+!x2)&!(x1&x3)))'
+test_formula = '((!x1*!x2*x3)+(!x1*x2*!x3)+(!x1*x2*x3)+(x1*x2*!x3))'
 
 
 if __name__ == '__main__':
-    minimizer = CalculationCNFMinimizer('(!((!x1+!x2)&!(x1&x3)))')
-    minimizer.build_reduced_cnf()
-    print(f'FCNF: {minimizer.non_minimized_fcnf}')
-    print(f'Reduced CNF: {minimizer.reduced_cnf}')
-    minimizer = CalculationDNFMinimizer('(!((!x1+!x2)&!(x1&x3)))')
-    minimizer.build_reduced_dnf()
-    print(f'FDNF: {minimizer.non_minimized_fdnf}')
-    print(f'Reduced DNF: {minimizer.reduced_dnf}')
+    print('CALCULATION MINIMIZATION METHOD'.center(60, ' '))
+    minimizer = CalculationMinimizer('(!((!x1+!x2)&!(x1&x3)))', mode='CNF')
+    minimizer.reduce_func()
+    print(f'FCNF: {minimizer.non_minimized_func}')
+    print(f'Reduced CNF: {minimizer.reduced_func}')
+    print(f'Minimized CNF: {minimizer.minimized_func}')
+    minimizer = CalculationMinimizer('(!((!x1+!x2)&!(x1&x3)))', mode='DNF')
+    minimizer.reduce_func()
+    print(f'FDNF: {minimizer.non_minimized_func}')
+    print(f'Reduced DNF: {minimizer.reduced_func}')
+    print(f'Minimized DNF: {minimizer.minimized_func}')
