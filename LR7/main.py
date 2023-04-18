ce4e55e2b115f6bec15d364ba03135cc4ac09d8c
Raman Zalomau <romchik349@gmail.com>
2023-04-18 21:09:15 +0300

    Done LR7

--- LR7/main.py
+++ LR7/main.py
@@ -2,6 +2,7 @@ from src.src import AssociativeProcessor
 
 
 if __name__ == '__main__':
-    associative_processor = AssociativeProcessor(3, 4)
+    associative_processor = AssociativeProcessor(3, 10)
     print(associative_processor)
     print(f'Search in range result: {associative_processor.search_within_given_range("001", "100")}')
+    print(f'Pattern search: {associative_processor.closest_pattern_search("00x")}')
--- LR7/src/src.py
+++ LR7/src/src.py
@@ -1,30 +1,27 @@
 import random
-from itertools import chain
 
 
 class AssociativeProcessor:
     def __init__(self, word_size: int, memory_matrix_size: int):
         self.__word_size = word_size
-        self.__memory_table = [['0' * word_size] * memory_matrix_size for _ in range(memory_matrix_size)]
+        self.__memory_table = ['0' * word_size] * memory_matrix_size
         self.__memory_matrix_size = memory_matrix_size
         self.__fill_memory_table()
 
     def __fill_memory_table(self):
         for i in range(self.__memory_matrix_size):
-            for j in range(self.__memory_matrix_size):
-                self.__memory_table[i][j] = ''.join([str(random.randint(0, 1)) for _ in range(self.__word_size)])
+            self.__memory_table[i] = ''.join([str(random.randint(0, 1)) for _ in range(self.__word_size)])
 
-    def __comparison_flags(self, memory_word: str, search_word: str, compared_digit: int = 0):
-        if compared_digit > self.__word_size - 1:
-            return {'g_flag': 0, 'l_flag': 0}
-        memory_word_digit = bool(int(memory_word[compared_digit]))
-        search_word_digit = bool(int(search_word[compared_digit]))
-        return {'g_flag': self.__comparison_flags(memory_word, search_word, compared_digit + 1)['g_flag'] or
-                          (not search_word_digit and memory_word_digit and not
-                           self.__comparison_flags(memory_word, search_word, compared_digit + 1)['l_flag']),
-                'l_flag': self.__comparison_flags(memory_word, search_word, compared_digit + 1)['l_flag'] or
-                          (search_word_digit and not memory_word_digit and not
-                           self.__comparison_flags(memory_word, search_word, compared_digit + 1)['g_flag'])}
+    def __comparison_flags(self, memory_word: str, search_word: str):
+        prev_g_flag, prev_l_flag = False, False
+        for i in range(self.__word_size):
+            memory_word_digit = bool(int(memory_word[i]))
+            search_word_digit = bool(int(search_word[i]))
+            next_g_flag = prev_g_flag or (not search_word_digit and memory_word_digit and not prev_l_flag)
+            next_l_flag = prev_l_flag or (search_word_digit and not memory_word_digit and not prev_g_flag)
+            prev_g_flag, prev_l_flag = next_g_flag, next_l_flag
+        return {'g_flag': prev_g_flag,
+                'l_flag': prev_l_flag}
 
     def __compare_words(self, memory_word: str, search_word: str) -> int:
         comparison_flags = self.__comparison_flags(memory_word, search_word)
@@ -38,14 +35,24 @@ class AssociativeProcessor:
             raise ValueError('Flags can\'t be equal to 1 at the same time')
 
     def search_within_given_range(self, lower_bound: str, upper_bound: str):
-        memory_table = list(chain.from_iterable(self.__memory_table))
-        search_words = [word for word in memory_table if self.__compare_words(word, upper_bound) == -1]
-        search_words += [word for word in memory_table if self.__compare_words(word, lower_bound) == 1]
+        search_words = [word for word in self.__memory_table if self.__compare_words(word, upper_bound) == -1]
+        search_words = list(filter(lambda word: self.__compare_words(word, lower_bound) == 1, search_words))
         return search_words
+
+    def closest_pattern_search(self, pattern: str):
+        difference_list = list()
+        for word in self.__memory_table:
+            difference_rank = 0
+            for i in range(self.__word_size):
+                difference_rank += 1 if word[i] != pattern[i] and pattern[i] != 'x' else 0
+            difference_list.append((word, difference_rank))
+        min_difference_rank = min([i[1] for i in difference_list])
+        difference_list = list(filter(lambda x: x[1] == min_difference_rank, difference_list))
+        return [i[0] for i in difference_list]
 
     def __str__(self):
         result_str = ''
-        result_str += 'Memory Table'.center(self.__memory_matrix_size * (1 + self.__word_size), ' ') + '\n'
-        for memory_row in self.__memory_table:
-            result_str += ' '.join(memory_row) + '\n'
+        result_str += 'Memory Array'.center(self.__word_size, ' ') + '\n'
+        for word in self.__memory_table:
+            result_str += word + '\n'
         return result_str
